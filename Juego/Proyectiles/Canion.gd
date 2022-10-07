d15a52bccdedbac721417af6cf8c4652cb48d30c
Tutorial <114967833+santy1023@users.noreply.github.com>
2022-10-07 19:06:12 -0300

    efectos visuales
    -se agregaron efectos visuales a los proyectiles

--- Juego/Proyectiles/Canion.gd
+++ Juego/Proyectiles/Canion.gd
@@ -14,30 +14,15 @@ onready var esta_enfriado:bool = true
 onready var esta_disparando:bool = false setget set_esta_disparando
 
 ## Atributos
-var puntos_disparos:Array = []
+var puntos_disparo:Array = []
 
-## setters y getters
+## Setters y Getters
 func set_esta_disparando(disparando: bool) -> void:
 	esta_disparando = disparando
 
-func disparar() -> void:
-	esta_enfriado = false
-	disparo_sfx.play()
-	timer_enfriamiento.start()
-	disparo_sfx.play()
-	for punto_disparo in puntos_disparos:
-		var new_proyectil:Proyectil = proyectil.instance()
-		new_proyectil.crear(
-			punto_disparo.global_position,
-			get_owner().rotation,
-			velocidad_proyectil,
-			danio_proyectil
-		)
-		Eventos.emit_signal("disparo", new_proyectil)
-
 ## Metodos
 func _ready() -> void:
-	alamcenar_puntos_disparo()
+	almacenar_puntos_disparo()
 	timer_enfriamiento.wait_time = cadencia_disparo
 
 func _process(_delta: float) -> void:
@@ -45,11 +30,25 @@ func _process(_delta: float) -> void:
 		disparar()
 
 ## Metodos custom
-func alamcenar_puntos_disparo() -> void:
+func almacenar_puntos_disparo() -> void:
 	for nodo in get_children():
 		if nodo is Position2D:
-			puntos_disparos.append(nodo)
+			puntos_disparo.append(nodo)
 
+func disparar() -> void:
+	esta_enfriado = false
+	disparo_sfx.play()
+	timer_enfriamiento.start()
+	disparo_sfx.play()
+	for punto_disparo in puntos_disparo:
+		var new_proyectil:Proyectil = proyectil.instance()
+		new_proyectil.crear(
+			punto_disparo.global_position,
+			get_owner().rotation,
+			velocidad_proyectil,
+			danio_proyectil
+			)
+		Eventos.emit_signal("disparo", new_proyectil)
 
 func _on_TimerEnfriamiento_timeout() -> void:
 	esta_enfriado = true
