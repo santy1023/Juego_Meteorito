e424e72da00d8c5908573be84a95198c34cccd41
Tutorial <114967833+santy1023@users.noreply.github.com>
2022-10-07 13:11:33 -0300

    Nuevos cambios
    -se modifico el sonido
    -modificaciones de codigos
    -creacion de un bus de sonido

--- Juego/Proyectiles/Canion.gd
+++ Juego/Proyectiles/Canion.gd
@@ -22,9 +22,9 @@ func set_esta_disparando(disparando: bool) -> void:
 
 func disparar() -> void:
 	esta_enfriado = false
-	disparo_sfx.play()
 	timer_enfriamiento.start()
 	for punto_disparo in puntos_disparos:
+		disparo_sfx.play()
 		var new_proyectil:Proyectil = proyectil.instance()
 		new_proyectil.crear(
 			punto_disparo.global_position,
