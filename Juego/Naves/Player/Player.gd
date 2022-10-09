a9c104842de2ed7af6edb5fe4e67ced4c0f4e456
Tutorial <114967833+santy1023@users.noreply.github.com>
2022-10-09 00:40:04 -0300

    clase 6
    clase 6 terminada

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -10,11 +10,11 @@ export var potencia_rotacion:int = 280
 export var estela_maxima:int = 150
 
 ## Atributos
+var estado_actual:int = ESTADO.SPAWN
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
-var estado_actual:int = ESTADO.SPAWN
 
-## Atributos onready
+## Atributos Onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
@@ -28,35 +28,32 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if not esta_input_activo():
 		return
-
-	#Disparo Rayo
+	
+	# Disparo Rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
 	
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
-
-# Control estela
+	
+	# Control estela y sonido motor
 	if event.is_action_pressed("mover_adelante"):
 		estela.set_max_points(estela_maxima)
 		motor_sfx.sonido_on()
 	elif event.is_action_pressed("mover_atras"):
 		estela.set_max_points(0)
 		motor_sfx.sonido_on()
-
+	
 	if (event.is_action_released("mover_adelante")
 		or event.is_action_released("mover_atras")):
 			motor_sfx.sonido_off()
 
-func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
-	rad2deg(rotation)
-	empuje.rotated(rotation)
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	player_input()
-	
 
 ## Metodos Custom
 func controlador_estados(nuevo_estado: int) -> void:
@@ -82,27 +79,27 @@ func controlador_estados(nuevo_estado: int) -> void:
 func player_input() -> void:
 	if not esta_input_activo():
 		return
-
-		# Empuje
-		empuje = Vector2.ZERO
-		if Input.is_action_pressed("mover_adelante"):
-			empuje = Vector2(potencia_motor, 0)
-		elif Input.is_action_pressed("mover_atras"):
-			empuje = Vector2(-potencia_motor, 0)
-
-		# Rotacion
-		dir_rotacion = 0
-		if Input.is_action_pressed("rotar_antihorario"):
-			dir_rotacion -= 1
-		elif Input.is_action_pressed("rotar_horario"):
-			dir_rotacion += 1
-
+	
+	# Empuje
+	empuje = Vector2.ZERO
+	if Input.is_action_pressed("mover_adelante"):
+		empuje = Vector2(potencia_motor, 0)
+	elif Input.is_action_pressed("mover_atras"):
+		empuje = Vector2(-potencia_motor, 0)
+	
+	# Rotacion
+	dir_rotacion = 0
+	if Input.is_action_pressed("rotar_antihorario"):
+		dir_rotacion -= 1
+	elif Input.is_action_pressed("rotar_horario"):
+		dir_rotacion += 1
+	
 	# Disparo
-		if Input.is_action_pressed("disparo_principal"):
-			canion.set_esta_disparando(true)
-			
-		if Input.is_action_just_released("disparo_principal"):
-			canion.set_esta_disparando(false)
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+		
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
 
 func esta_input_activo() -> bool:
 	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
@@ -110,11 +107,10 @@ func esta_input_activo() -> bool:
 	
 	return true
 
+func destruir() -> void:
+	controlador_estados(ESTADO.MUERTO)
+
 ## Señales internas
-
-
-
-
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "spawn":
 		controlador_estados(ESTADO.VIVO)
