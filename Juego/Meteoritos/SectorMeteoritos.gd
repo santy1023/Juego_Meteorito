79e8fde4d2907d8a44fac4d1bea182d116311683
Tutorial <114967833+santy1023@users.noreply.github.com>
2022-10-21 11:12:40 -0300

    clase 2
    se completo la clase 2

--- Juego/Meteoritos/SectorMeteoritos.gd
+++ Juego/Meteoritos/SectorMeteoritos.gd
@@ -1,15 +1,23 @@
 class_name SectorMeteoritos
 extends Node2D
 
-## Atributos export
-export var cantidad_meteoritos:int = 10
+## Atributos 
+var cantidad_meteoritos:int = 10
+var intervalo_spawn:float = 1.2
 
 ## Atributos
 var spawners:Array
 
 ## Metodos
 func _ready() -> void:
+	$Timer.wait_time = intervalo_spawn
 	almacenar_spawners()
+	conectar_seniales_detectores()
+
+## Contructor
+func crear(pos: Vector2, meteoritos: int) -> void:
+	global_position = pos
+	cantidad_meteoritos = meteoritos
 
 ## Metodos Custom
 func almacenar_spawners() -> void:
@@ -20,10 +28,17 @@ func spawner_aleatorio() -> int:
 	randomize()
 	return randi() % spawners.size()
 
+func conectar_seniales_detectores() -> void:
+	for detector in $DetectorFueraZona.get_children():
+		detector.connect("body_entered", self, "_on_detector_body_entered")
+
 ## Señales Internas
 func _on_SpawnTimer_timeout() -> void:
 	if cantidad_meteoritos == 0:
-		$SpawnTimer.stop()
+		$Timer.stop()
 		return
 	spawners[spawner_aleatorio()].spawnear_meteorito()
 	cantidad_meteoritos -= 1
+
+func _on_detector_body_entered(body: Node) -> void:
+	body.set_esta_en_sector(false)
--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -5,10 +5,15 @@ extends Node2D
 export var explosion:PackedScene = null
 export var meteorito:PackedScene = null
 export var explosion_meteorito:PackedScene = null
+export var sector_meteoritos:PackedScene = null
+
+export var tiempo_transicion_camara:float = 5.0
 
 ## Atributos Onready
 onready var contenedor_proyectiles:Node
 onready var contenedor_meteoritos:Node
+onready var contenedor_sector_meteoritos:Node
+onready var camara_nivel:Camera2D = $CamaraNivel
 
 ## Metodos
 func _ready() -> void:
@@ -19,6 +24,7 @@ func _ready() -> void:
 func conectar_seniales() -> void:
 	Eventos.connect("disparo", self, "_on_disparo")
 	Eventos.connect("nave_destruida", self, "_on_nave_destruida")
+	Eventos.connect("nave_en_sector_peligro", self, "_on_nave_en_sector_peligro")
 	Eventos.connect("spawn_meteorito", self, "_on_spawn_meteoritos")
 	Eventos.connect("meteorito_destruido", self, "_on_meteorito_destruido")
 
@@ -30,6 +36,9 @@ func crear_contenedores() -> void:
 	contenedor_meteoritos = Node.new()
 	contenedor_meteoritos.name = "ContenedorMeteoritos"
 	add_child(contenedor_meteoritos)
+	contenedor_sector_meteoritos = Node.new()
+	contenedor_sector_meteoritos.name = "ContenedorSectorMeteoritos"
+	add_child(contenedor_sector_meteoritos)
 
 func _on_disparo(proyectil:Proyectil) -> void:
 	add_child(proyectil)
@@ -41,6 +50,36 @@ func _on_nave_destruida(posicion: Vector2, num_explosiones: int) -> void:
 		add_child(new_explosion)
 		yield(get_tree().create_timer(0.6),"timeout")
 
+func _on_nave_en_sector_peligro(centro_cam:Vector2, tipo_peligro:String, num_peligros:int) -> void:
+	if tipo_peligro == "Meteorito":
+		crear_sector_meteoritos(centro_cam, num_peligros)
+	elif tipo_peligro == "Enemigo":
+		pass
+
+func crear_sector_meteoritos(centro_camara:Vector2, numero_peligros:int) -> void:
+	var new_sector_meteoritos:SectorMeteoritos = sector_meteoritos.instance()
+	new_sector_meteoritos.crear(centro_camara, numero_peligros)
+	camara_nivel.global_position = centro_camara
+	camara_nivel.current = true
+	contenedor_sector_meteoritos.add_child(new_sector_meteoritos)
+	transicion_camaras(
+		$Player/CameraPlayer.global_position,
+		camara_nivel.global_position,
+		camara_nivel)
+
+func transicion_camaras(desde: Vector2, hasta: Vector2, camara_actual: Camera2D) -> void:
+	$TweenCamara.interpolate_property(
+		camara_actual,
+		"global_position",
+		desde,
+		hasta,
+		tiempo_transicion_camara,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_IN_OUT
+	)
+	camara_actual.current = true
+	$TweenCamara.start()
+	
 func _on_meteorito_destruido(pos: Vector2) -> void:
 	var new_explosion:ExplosionMeteorito = explosion_meteorito.instance()
 	new_explosion.global_position = pos
